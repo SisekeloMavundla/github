ccd679a0d0cdc775c27a3cd3d480748493ac6c36
RC_Student_lab <RC_Student_lab@10.0.0.106>
2024-09-10 11:32:20 +0000

    First comment

---
+++ src/main/java/com/mycompany/githubtesting/GitHubTesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubtesting;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class GitHubTesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
